5cb3f412b250dc4571b5140b96229514cb4e9486
ming-sil <choming4926@gmail.com>
2022-07-20 12:05:37 +0900

    create components & Router setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Signup } from "./components/SignUp";
+
 function App() {
-  return <>Sign_in</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
