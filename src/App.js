55e7fa446cc0b2b884dbc3a32142a864708f95d8
ming-sil <choming4926@gmail.com>
2022-07-20 12:21:00 +0900

    style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { Signup } from "./components/SignUp";
+import { GlobalStyled } from "./style/GlobalStyled";
 
 function App() {
   return (
     <Router>
+      <GlobalStyled />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
---
+++ src/style/GlobalStyled.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyled = createGlobalStyle`
+
+    ${reset}
+
+    *{box-sizing: border-box}
+    
+`;
